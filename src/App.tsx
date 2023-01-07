733dcae8c31276acae3903fece8bca57c932048a
PlatonVV <platonvolodin@icloud.com>
2023-01-07 18:44:51 -0500

    Create state test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+
+    type MessageType = {
+        message: string;
+    }
+
+    const HelloMessage: React.FC<MessageType> =(props) => {
+        return (
+            <>
+                <h1>{props.message}</h1>
+            </>
+        )
+    }
+    const ByeMessage: React.FC<MessageType> = (props) => {
+        return (
+            <>
+                <h1>{props.message}</h1>
+            </>
+        )
+    }
+
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Route path={"/hello"} render={() => <HelloMessage message='HelloMessage'/> }/>
+                <Route path={"/bye"} render={() => <ByeMessage message='Bye-Bye!!'/>}/>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/redux/state.ts
@@ -0,0 +1,31 @@
+let state = {
+    profilePage: {
+        posts: [
+            {id: 1, message: 'Hi, how are you?', likesCount: 12},
+            {id: 2, message: 'This is my first post', likesCount: 11},
+            {id: 3, message: 'Yo Yo Yo Yo', likesCount: 1},
+            {id: 3, message: 'Bla bla bla ', likesCount: 7},
+        ]
+    },
+    dialogsPage: {
+        dialogs: [
+            {id: 1, name: 'Plato'},
+            {id: 2, name: 'Ivan'},
+            {id: 3, name: 'Igor'},
+            {id: 4, name: 'Nikita'},
+            {id: 5, name: 'Cris'},
+            {id: 6, name: 'Neo'},
+        ],
+        messages: [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'How are you?'},
+            {id: 3, message: 'Whats up!'},
+            {id: 4, message: 'V-vendetta'},
+            {id: 5, message: 'YoYOYO'},
+            {id: 6, message: 'Carnaval jazz!'},
+        ]
+    },
+    sidebar: {}
+}
+
+export default state;
