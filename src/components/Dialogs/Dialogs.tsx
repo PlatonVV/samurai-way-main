34a41dae6cb6e950fb6c606cdad037e03d2b7aae
PlatonVV <platonvolodin@icloud.com>
2023-02-02 12:24:55 -0500

    part 21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import s from './Dialogs.module.css'
 
-export const Dialogs = (props: any) => {
+export const Dialogs = () => {
     return (
-        <>Dialogs</>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dimych
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Mario
+                </div>
+                <div className={s.dialog}>
+                    Vince
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}>Hi, how are you?</div>
+                <div className={s.dialog}>Whats up?</div>
+                <div className={s.dialog}>Yo yo yo!</div>
+            </div>
+        </div>
     )
 }
