db83afa6036c8555ddc6901a0e25b4a4f5dfabfa
PlatonVV <platonvolodin@icloud.com>
2023-07-13 12:47:50 -0400

    some refactoring

--- src/components/ButtonOpt.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const ButtonOpt = () => {
-    return (
-        <div>
-
-        </div>
-    );
-};
--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -8,16 +8,12 @@ type PropsType = {
 
 export const Post = (props: PropsType) => {
     return (
-        <div>
             <div className={s.item}>
                 <img
                     src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/will-smith-unfinshed-andrew-read.jpg"
                     alt="Will Smith"/>
                 {props.message}
                 <div>{`${props.likesCount} like`}</div>
-                {/*<div>like</div>*/}
             </div>
-        </div>
-
     )
 }
